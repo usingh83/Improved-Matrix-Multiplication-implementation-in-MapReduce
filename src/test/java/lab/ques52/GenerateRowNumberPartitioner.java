0ddc558f98f206230b5f27d2e629b4a1e2621e88
Uday Singh <kanwarudaysingh@gmail.com>
2018-08-06 20:25:04 -0500

    Final Commit

---
+++ src/test/java/lab/ques52/GenerateRowNumberPartitioner.java
@@ -0,0 +1,24 @@
+package lab.ques52;
+
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapred.Partitioner;
+import org.apache.hadoop.mapred.JobConf;
+
+public class GenerateRowNumberPartitioner implements Partitioner<Text, Text>
+{
+  public int getPartition(Text key, Text value, int numReduceTasks)
+  {
+    String [] wcTokens = key.toString().split(", ");
+    
+    int returnValue = (wcTokens[0].hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+
+    return returnValue;
+  }
+  
+  public void configure(JobConf job)
+  {
+    
+  }
+  
+  }
